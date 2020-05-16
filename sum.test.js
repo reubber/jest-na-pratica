85079610cc0e165f6c8eb3a3b70220f954ae72a7
reubber <reubbers@alu.ufc.br>
2020-05-16 20:25:53 -0300

    add dev-dependency

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
-'use strict'
+import { expect } from 'chai'
+import sum from './sum'
 
-it('Testando Jest', () => {
+it('sum is a function?', () => {
+  expect(sum).to.be.a('function')
+})
 
+it('sum(1,2) should return 3', () => {
+  expect(sum(1, 2)).to.be.equal(3)
 })
